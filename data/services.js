f9d072ea1897e158314e630c9803812bed308fda
zakve <zaklas.martin@gmail.com>
2022-11-08 09:11:09 +0100

    Add services section

---
+++ data/services.js
@@ -0,0 +1,14 @@
+export const services = [
+    {
+        title: "Mobile apps",
+        img: "https://images.unsplash.com/photo-1480694313141-fce5e697ee25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    },
+    {
+        title: "Web apps",
+        img: "https://images.unsplash.com/photo-1559028006-448665bd7c7f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+    },
+    {
+        title: "Design",
+        img: "https://images.unsplash.com/photo-1618788372246-79faff0c3742?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80"
+    }
+] 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { Container, Card, Row, Text, Navbar, Image } from "@nextui-org/react";
+import { Card, Text, Navbar, Col, Grid } from "@nextui-org/react";
 
 import Hero from './hero'
+import { services } from '../data/services'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -15,19 +16,44 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <Navbar shouldHideOnScroll>
-          <Navbar.Content hideIn="xs">
-            <Navbar.Link href="#">Home</Navbar.Link>
-            <Navbar.Link isActive href="#">Services</Navbar.Link>
-            <Navbar.Link href="#">Portfolio</Navbar.Link>
-            <Navbar.Link href="#">Hire me</Navbar.Link>
-          </Navbar.Content>
-        </Navbar>
+        <nav>
+          <Navbar shouldHideOnScroll>
+            <Navbar.Content hideIn="xs">
+              <Navbar.Link href="#">Home</Navbar.Link>
+              <Navbar.Link isActive href="#">Services</Navbar.Link>
+              <Navbar.Link href="#">Portfolio</Navbar.Link>
+              <Navbar.Link href="#">Hire me</Navbar.Link>
+            </Navbar.Content>
+          </Navbar>
+        </nav>
         <Hero />
         <section
           className={styles.services}
         >
-          <h2>Services</h2>
+          <h2>How can I help you?</h2>
+          <Grid.Container gap={2} justify="center">
+            {
+              services?.map((service, i) =>
+                <Grid xs={4} key={i}>
+                  <Card>
+                    <Card.Header>
+                      <Col>
+                        <Text h4 color="white">
+                          {service.title}
+                        </Text>
+                      </Col>
+                    </Card.Header>
+                    <Card.Image
+                      src={service.img}
+                      objectFit="cover"
+                      width="100%"
+                      height={140}
+                      alt="Card image background"
+                    />
+                  </Card>
+                </Grid>)
+            }
+          </Grid.Container>
         </section>
       </main>
 
