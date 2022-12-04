b862dfc85d4e568f1e4bd3b383b01d3106d1f2a8
zakve <zaklas.martin@gmail.com>
2022-12-04 11:03:05 +0100

    Update services

--- data/services.js
+++ data/services.js
@@ -1,14 +1,20 @@
 export const services = [
     {
         title: "Mobile apps",
-        img: "https://images.unsplash.com/photo-1480694313141-fce5e697ee25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+        img: "https://images.unsplash.com/photo-1480694313141-fce5e697ee25?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        description: ["Custom mobile app development", "Available on Google play and Apple store", "Low maintenance cost"],
+        badges: ["React Native", "Android", "iOS", "REST"]
     },
     {
         title: "Web apps",
-        img: "https://images.unsplash.com/photo-1559028006-448665bd7c7f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+        img: "https://images.unsplash.com/photo-1559028006-448665bd7c7f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+        description: ["Custom web app development", "React, Vue.js or Anglular", "Autodeployed to Vercel / Netlify"],
+        badges: ["HTML3", "CSS3", "React", "Vue.js", "Node.js", "REST", "Vercel/Netlify"]
     },
     {
         title: "Design",
-        img: "https://images.unsplash.com/photo-1618788372246-79faff0c3742?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80"
+        img: "https://images.unsplash.com/photo-1618788372246-79faff0c3742?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80",
+        description: ["Web or mobile app design", "Responsive multiplatform", "Logo, posters, ..."],
+        badges: ["Wireframes", "Web", "Mobile", "Logo", "Posters", "Figma", "Adobe CC"]
     }
 ] 
