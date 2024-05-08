42943201f11cae222ef4f62580f38087b701d28c
zakve <zaklas.martin@gmail.com>
2024-05-08 10:55:14 +0200

    feat: add title border

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,7 +1,14 @@
 export default function Title({ children }: { children: React.ReactNode }) {
-    return (
-        <h2 className="mt-10 mb-6 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors">
-            {children}
-        </h2>
-    )
+  return (
+    <div className="flex flex-col justify-center mb-6">
+      <h2
+        className={
+          "mt-10 mb-3 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors"
+        }
+      >
+        {children}
+      </h2>
+      <div className="w-10 m-auto h-1 bg-blue-900"></div>
+    </div>
+  );
 }
